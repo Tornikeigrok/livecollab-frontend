7d5d97b072621bc490f40866786844cc6d403734
Tornike Sharikadze <tokoahari@gmail.com>
2026-01-19 20:53:50 -0500

    Add Vercel Analytics to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
  <BrowserRouter>
 
     <App />
-
+   <Analytics />
  </BrowserRouter>
 )
